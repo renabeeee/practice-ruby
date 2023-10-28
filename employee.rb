41a0ba3ae169bf64f0e51cc25fa99ed399926926
Serena Green <serenafgreen@gmail.com>
2023-10-27 20:31:37 -0700

    adds employees class

--- employee.rb
+++ employee.rb
@@ -1,5 +1,14 @@
-def hello
-  puts "It's Friyay babbbby"
+class Employee
+	def initialize (input_first_name, input_last_name, input_salary, input_active)
+	@first_name = input_first_name
+	@last_name = input_last
+	@salay
 end
 
-hello
+def print_info
+		return "#{@first_name} #{@last_name} makes #{@salary} a year."
+	end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+puts employee1.print_info
