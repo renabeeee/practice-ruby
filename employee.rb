35fdf0c7c83488d179c92abaca5f9b33de6f20d6
Serena Green <serenafgreen@gmail.com>
2023-10-27 21:07:57 -0700

    adds yearly salary increase

--- employee.rb
+++ employee.rb
@@ -1,14 +1,21 @@
 class Employee
 	def initialize (input_first_name, input_last_name, input_salary, input_active)
 	@first_name = input_first_name
-	@last_name = input_last
-	@salay
+	@last_name = input_last_name
+  @salary = input_salary
+  @active = input_active
+end
+
+def yearly_salary_increase
+  @salary = 1.05 * @salary
 end
 
 def print_info
-		return "#{@first_name} #{@last_name} makes #{@salary} a year."
-	end
+  yearly_salary_increase
+  return "#{@first_name} #{@last_name} makes #{@salary} a year."
 end
 
-employee1 = Employee.new("Majora", "Carter", 80000, true)
+end
+
+employee1 = Employee.new("Majora", "Carter", 100000, true)
 puts employee1.print_info
